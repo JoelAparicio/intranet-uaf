b94d4e573deccc2882cf8b2ce7d18631833b7633
Joel Aparicio <japaricio@uaf.gob.pa>
2025-07-03 13:44:15 -0500

    cambio de nombre y logo

--- intranet-frontend/vue.config.js
+++ intranet-frontend/vue.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
     transpileDependencies: true,
+    // Configuración del título de la aplicación
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'UAF Intranet'
+                return args
+            })
+    },
+    // Configuración del servidor de desarrollo
     devServer: {
         host: '0.0.0.0',
         port: 3000,
